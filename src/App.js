434c53a7c1767fe86ce3a82f5b1ec8a208569a81
Cezar Augusto Crummenauer <crummenauerca@gmail.com>
2021-02-17 21:33:54 -0300

    Passing the component state down

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
 import Header from './components/Header'
 import Tasks from './components/Tasks'
 
+import { useState } from 'react'
+
 function App() {
+  const [tasks] = useState([{
+    id: 1,
+    text: 'Doctors appointment',
+    day: 'Feb 5th at 02:00pm',
+    reminder: true
+  }, {
+    id: 2,
+    text: 'Meeting at School',
+    day: 'Feb 6th at 01:30pm',
+    reminder: true
+  }, {
+    id: 3,
+    text: 'Food Shopping',
+    day: 'Feb 7th at 02:30pm',
+    reminder: false
+  }])
+
+  const deleteTask = id => {
+    console.log('delete', id)
+  }
+
   return (
     <div className="container">
-      <Header title='Hello Header' />
-      <Tasks />
+      <Header title='Task Tracker' />
+      <Tasks tasks={tasks} onDelete={deleteTask} />
     </div>
   )
 }
--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,40 +1,11 @@
-import { useState } from 'react'
-import Button from './Button'
+import Task from './Task'
 
-const Tasks = () => {
-  const [tasks, setTasks] = useState([{
-    id: 1,
-    text: 'Doctors appointment',
-    day: 'Feb 5th at 02:00pm',
-    reminder: true
-  }, {
-    id: 2,
-    text: 'Meeting at School',
-    day: 'Feb 6th at 01:30pm',
-    reminder: true
-  }, {
-    id: 3,
-    text: 'Food Shopping',
-    day: 'Feb 7th at 02:30pm',
-    reminder: false
-  }])
-
-  const addTask = event => {
-    setTasks([...tasks, {
-      id: 4,
-      text: 'Clothes Shopping',
-      day: 'Feb 8th at 04:20pm',
-      reminder: false
-    }])
-  }
-
+const Tasks = ({tasks, onDelete}) => {
   return (
     <>
       {tasks.map((task) => (
-        <h3 key={task.id}>{task.text}</h3>)
-      )}
-
-      <Button color='white' backgroundColor='blue' text='Add task' onClick={addTask} />
+        <Task key={task.id} task={task} onDelete={onDelete} />
+      ))}
     </>
   )
 }
